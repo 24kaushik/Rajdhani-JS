9f7a64db7e217ef5a789acf82acdeba40a53c35f
Kaushik <akaushikarkar@gmail.com>
2024-10-25 20:22:26 +0530

    fixed some issues while exporting

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
 import App from './server';
 
-// -------------------TESTING! Not to be used in production.------------------- //
-const server = new App();
-
-server.get("/", (_, res) => {
-  res.status(201).setHeaders({"Content-Length": "12"}).end("Hello World!");
-});
-server.get("/ks", (_, res) => {
-  res.end("Hello Kaushik!");
-});
-server.get("/json", (_, res) => {
-  res.json({a:1, b:2, c:3});
-});
-
-server.post("/", (req, res) => {
-  res.end(`${JSON.stringify(req.headers)}`);
-});
-
-server.listen(3000, "localhost");
-// --------------------------------------------------------------------------- //
-
-export default App;
+export = App;
